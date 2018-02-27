d33ef722cd7803b4ee377eeb87f9d87d47d63153
Ikelink <34509338+Ikelink@users.noreply.github.com>
2018-02-27 23:49:33 +0100

    Update JeuDeLaVie.java

--- Jeu-de-la-vie/src/JeuDeLaVie.java
+++ Jeu-de-la-vie/src/JeuDeLaVie.java
@@ -25,6 +25,43 @@ public class JeuDeLaVie extends JFrame{
 
         go(list, nb_generation);
     }
+    
+     private ListeChainee<Couple> Generation(ListeChainee<Couple> list){
+
+        ListeChainee<Couple> newGeneration = new ListeChainee<>();
+        Couple ATester = list.getTete().getValeur();
+        //on effectue la recherche du nombre de voisin pour ATester
+
+        while (ATester != null) {
+
+            int nbVoisin = 0;
+
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+
+                    if (i != 0 && j != 0) { //i=0 j=0 correspond au Couple ATester
+
+                        Couple voisin = new Couple(ATester.getX() + i, ATester.getY() + j);
+
+                        if (list.contains(voisin) == true) {
+                            nbVoisin += 1;
+                        }
+                    }
+                }
+            }
+
+            if (nbVoisin >= 2) {
+                newGeneration.add(ATester);
+            }
+
+            // ATester = ???? ;
+            // on change la valeur de ATester
+
+        }
+
+        return newGeneration;
+
+    }
 
     private void go(ListeChainee<Couple> list_depart, int nb_generation) {
 
