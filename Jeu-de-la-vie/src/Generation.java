84609f9ba4770ebeac62ce8a8e732bacfd6e451e
Ikelink <34509338+Ikelink@users.noreply.github.com>
2018-02-28 10:03:01 +0100

    Add files via upload

--- Jeu-de-la-vie/src/Generation.java
+++ Jeu-de-la-vie/src/Generation.java
@@ -5,36 +5,43 @@ public class Generation {
     private ListeChainee<Couple> newGeneration(ListeChainee<Couple> list){
 
         ListeChainee<Couple> newGeneration = new ListeChainee<>();
-        Couple ATester = list.getTete().getValeur();
-        //on effectue la recherche du nombre de voisin pour ATester
+        Couple reference = list.getTete().getValeur();
+        //on va se servir d'un maillon de reference pour gerer les naissance des cellules vides aux alentours de celle-ci
+        //les cellules de "references" sont les cellules de la liste initiale
+
 
         Iterator it = list.iterator();
         while (it.hasNext()) {
 
-            int nbVoisin = 0;
-
-            for (int i = -1; i <= 1; i++) {
+         for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
+                    int nbVoisin = 0;
 
                     if (i != 0 && j != 0) { //i=0 j=0 correspond au Couple ATester
+                        Couple ATester = new Couple(reference.getX() + i, reference.getY() + j);
 
-                        Couple voisin = new Couple(ATester.getX() + i, ATester.getY() + j);
-
-                        if (list.contains(voisin) == true) {
-                            nbVoisin += 1;
+                        for (int k = -1; k <= 1; k++) {
+                            for (int l = -1; l <= 1; l++) {
+                                Couple voisin = new Couple(ATester.getX() +k, ATester.getY() +l);
+                                if (i != 0 && j != 0 && list.contains(voisin) == true) {
+                                    nbVoisin += 1;
+                                }
+                            }
                         }
-                    }
+
+                        if (nbVoisin >= 2) {
+                            newGeneration.add(ATester);
+                            //la cellule possede au moins 2 voisins, il y a donc une naissance sur cette case
+                        }
+
+                    } 
                 }
             }
 
-            if (nbVoisin >= 2) {
-                newGeneration.add(ATester);
-            }
+            reference = (Couple)it.next() ;
+            // on change le maillon de reference
 
-            ATester = (Couple)it.next() ;
-            // on change la valeur de ATester
-
-        }
+        } // fin boucle while
 
         return newGeneration;
 
