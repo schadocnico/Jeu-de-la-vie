e0278df7a09ada80bd99b643a15b8fb0abc10bfa
Nicolas <schadocnico@gmail.com>
2018-03-21 02:29:56 +0100

    Add files via upload
    Opti de code mineur.

--- Jeu-de-la-vie/src/Generation.java
+++ Jeu-de-la-vie/src/Generation.java
@@ -2,50 +2,44 @@ import java.util.Iterator;
 
 public class Generation {
 
-    public ListeChainee<Couple> newGeneration(ListeChainee<Couple> list){
+    public static ListeChainee<Couple> newGeneration(ListeChainee<Couple> list){
 
         ListeChainee<Couple> newGeneration = new ListeChainee<>();
         ListeChainee<Couple> dejaFait = new ListeChainee<>();
-        Couple reference = list.premierElement();
+        Couple reference;
         //on va se servir d'un maillon de reference pour gerer les naissance des cellules vides aux alentours de celle-ci
         //les cellules de "references" sont les cellules de la liste initiale
 
-
         Iterator it = list.iterator();
         while (it.hasNext()) {
+            reference = (Couple)it.next() ;
 
-         for (int i = -1; i <= 1; i++) {
+            for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
+                    Couple ATester = new Couple(reference.getX() + i, reference.getY() + j);
 
-                        Couple ATester = new Couple(reference.getX() + i, reference.getY() + j);
-
-                        if (dejaFait.contains(ATester) != true){
+                    if (!dejaFait.contains(ATester)){
                         dejaFait.add(ATester); // on exclut les couples dont on a deja verifié les alentours pour le prochain tour de boucle
-
-                        ListeChainee<Couple> listeChainee2 = list.selection((o -> {
-                                     if (o.getX() == ATester.getX()-1 || o.getX() == ATester.getX()+1 || o.getX() == ATester.getX())
-                                         return (o.getY() == ATester.getY()-1 || o.getY() == ATester.getY()+1 || o.getY() == ATester.getY();
-                                     else return false;
-                                     }));
-
+                        ListeChainee<Couple> listeChainee2 = list.selection(((Couple o) ->
+                                (o.getX() == ATester.getX() - 1 || o.getX() == ATester.getX() + 1 || o.getX() == ATester.getX())
+                            && (o.getY() == ATester.getY() - 1 || o.getY() == ATester.getY() + 1 || o.getY() == ATester.getY())
+                        ));
                         int size=listeChainee2.getSize();
-                        if (reference.equals(ATester))
+                        boolean estVivant = listeChainee2.contains(ATester);
+                        if (estVivant)
                             size--;
 
-                        if (size ==2 || (size==3 && list.contains(ATester))){
-                            
-                               newGeneration.add(ATester);
-                             //la cellule possede au moins 2 voisins, il y a donc une naissance sur cette case
+                        if (size == 3 || (size==2 && estVivant) ){
+                            newGeneration.add(ATester);
+                            //la cellule possede au moins 2 voisins, il y a donc une naissance sur cette case
                         }
 
 
-                        }
+                    }
 
-                      } //if
+                } //if
 
             }
-
-            reference = (Couple)it.next() ;
             // on change le maillon de reference
 
         } // fin boucle while
